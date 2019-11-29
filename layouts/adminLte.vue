667b9001383f5b91b1c77b796f1393542a730a35
sonrapin <sonramname@hotmail.com>
2019-11-29 10:45:51 +0700

    First Commit

---
+++ layouts/adminLte.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NavBar />
+    <SideBar />
+    <nuxt class="content-wrapper" />
+    <Footer />
+  </div>
+</template>
+<script>
+import NavBar from '../components/page/NavBar'
+import SideBar from '../components/page/SideBar'
+import Footer from '../components/page/Footer'
+export default {
+  components: {
+    NavBar, SideBar, Footer
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div class="content-header" />
+
+    <section class="content" />
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'adminLte',
+  components: {}
+}
+</script>
